342b1844ae21c8d99fe675a0f91ae2c87369ce0d
André devkcud Albanese <patommmmm@proton.me>
2023-11-07 20:03:56 -0300

    tarefa: Implementa página Home

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
+import CardSaibaMais from "@/components/CardSaibaMais";
+import CardTopico from "@/components/CardTopico";
 import Carousel from "@/components/Carousel";
+import Separator from "@/components/Separator";
 import Image from "next/image";
+
+import { BiBookAlt } from "react-icons/bi";
+
+import "./home.css";
 
 export const metadata = {
   title: "Projeto Faísca - Home",
@@ -15,6 +22,51 @@ export default function Home() {
           <Image key={i} src={s} width={1000} height={250} alt="Placeholder" />
         ))}
       </Carousel>
+
+      <Separator>Recém-Lançados</Separator>
+
+      <div className="cards">
+        <CardSaibaMais link="" image="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </CardSaibaMais>
+        <CardSaibaMais link="" image="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam omnis
+          assumenda consectetur est perferendis exercitationem mollitia.
+        </CardSaibaMais>
+        <CardSaibaMais link="" image="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam omnis
+          assumenda consectetur est perferendis exercitationem mollitia
+          laudantium? Temporibus autem, itaque perferendis aliquid vel omnis
+          possimus eveniet tempora rem!
+        </CardSaibaMais>
+        <CardSaibaMais link="" image="/placeholder.jpg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </CardSaibaMais>
+        <CardSaibaMais link="" image="/placeholder.jpg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam omnis
+          assumenda consectetur est perferendis exercitationem mollitia.
+        </CardSaibaMais>
+        <CardSaibaMais link="" image="/placeholder.jpg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam omnis
+          assumenda consectetur est perferendis exercitationem mollitia
+          laudantium? Temporibus autem, itaque perferendis aliquid vel omnis
+          possimus eveniet tempora rem!
+        </CardSaibaMais>
+      </div>
+
+      <Separator>Mais Populares</Separator>
+
+      <div className="topics">
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+        <CardTopico icon={<BiBookAlt />}>Projeto</CardTopico>
+      </div>
     </section>
   );
 }
