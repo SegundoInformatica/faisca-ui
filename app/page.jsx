8e5c048093010aa2ef352a2451d85bf9f39406a8
André devkcud Albanese <patommmmm@proton.me>
2023-11-07 17:55:35 -0300

    recurso: Adiciona carrossel na `Home`

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,20 @@
+import Carousel from "@/components/Carousel";
+import Image from "next/image";
+
 export const metadata = {
   title: "Projeto Faísca - Home",
 };
 
+const slides = ["/placeholder.jpg", "/placeholder.jpg", "/placeholder.jpg"];
+
 export default function Home() {
   return (
     <section className="home">
+      <Carousel>
+        {slides.map((s, i) => (
+          <Image key={i} src={s} width={1000} height={250} alt="Placeholder" />
+        ))}
+      </Carousel>
     </section>
   );
 }
