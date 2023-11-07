868669d7d8159b02acb3b0bee36306d43467cfc6
André devkcud Albanese <patommmmm@proton.me>
2023-11-07 16:06:39 -0300

    recurso: Adiciona header e footer
    Todas as páginas herdarão este header e footer.

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,16 @@
-import { Inter } from "next/font/google";
+import Image from "next/image";
+import { Roboto } from "next/font/google";
+
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { FiMenu } from "react-icons/fi";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export const metadata = {
   title: "Projeto Faísca",
@@ -9,8 +18,53 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt">
+      <body className={roboto.className}>
+        <header className="full-bleed">
+          <Image src="/logo.svg" width={150} height={37.5} alt="Logo" />
+
+          <div className="wrapper">
+            <section className="search-bar">
+              <input type="text" placeholder="Digite o que você procura" />
+              <button>
+                <FaMagnifyingGlass />
+              </button>
+            </section>
+
+            <button className="hamburguer-menu">
+              <FiMenu />
+            </button>
+          </div>
+        </header>
+
+        <main>{children}</main>
+
+        <footer className="full-bleed">
+          <Image src="/logo.svg" width={150} height={37.5} alt="Logo" />
+
+          <ul className="links">
+            <li>
+              <a href="">Sobre</a>
+            </li>
+            <li>
+              <a href="">Políticas e Privacidade</a>
+            </li>
+            <li>
+              <a href="">Fale Conosco</a>
+            </li>
+          </ul>
+
+          <ul className="social">
+            <li>
+              <a href="https://www.instagram.com/faiscacientifica">
+                <AiOutlineInstagram />
+              </a>
+            </li>
+          </ul>
+
+          <p className="copyright">Projeto Faísca © 2023</p>
+        </footer>
+      </body>
     </html>
   );
 }
