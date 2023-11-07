86f2a25f14e0bc14cd0369015d5ed28bdd1eb83d
André devkcud Albanese <patommmmm@proton.me>
2023-11-07 19:29:20 -0300

    recurso: Adiciona componente Separator Componente para separar sessões da página

---
+++ components/Separator.jsx
@@ -0,0 +1,11 @@
+import "./Separator.css";
+
+export default function Separator({ children }) {
+  return (
+    <div className="separator__wrapper">
+      <div className="separator__line" />
+      <div className="separator__content">{children}</div>
+      <div className="separator__line" />
+    </div>
+  );
+}
