d568324a81849b375344a5c8ffa6e5a0a3d5094a
André devkcud Albanese <patommmmm@proton.me>
2023-12-09 17:15:45 -0300

    estilo: Atualiza espaçamento e line-breaking

--- components/ArtigoHeader.jsx
+++ components/ArtigoHeader.jsx
@@ -8,8 +8,7 @@ export default function ArtigoHeader({ titulo, resumo }) {
       <section className="textual">
         <h1>{titulo}</h1>
 
-        <article className="audio-control">
-        </article>
+        <article className="audio-control"></article>
 
         <article className="resumo">
           <h2>Resumo</h2>
@@ -19,9 +18,24 @@ export default function ArtigoHeader({ titulo, resumo }) {
       </section>
 
       <section className="visual">
-        <Image src="/placeholder.jpg" width={300} height={240} alt="Placeholder" />
-        <Image src="/placeholder.jpg" width={300} height={240} alt="Placeholder" />
-        <Image src="/placeholder.jpg" width={300} height={480} alt="Placeholder" />
+        <Image
+          src="/placeholder.jpg"
+          width={300}
+          height={240}
+          alt="Placeholder"
+        />
+        <Image
+          src="/placeholder.jpg"
+          width={300}
+          height={240}
+          alt="Placeholder"
+        />
+        <Image
+          src="/placeholder.jpg"
+          width={300}
+          height={480}
+          alt="Placeholder"
+        />
       </section>
     </div>
   );
