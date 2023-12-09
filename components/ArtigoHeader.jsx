c5d8ee10381f083dc89460104bfd89721ed1cdfc
André devkcud Albanese <patommmmm@proton.me>
2023-12-09 17:02:15 -0300

    tarefa: Melhora a organização do componente `ArtigoHeader`

---
+++ components/ArtigoHeader.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+import "./ArtigoHeader.css";
+
+export default function ArtigoHeader({ titulo, resumo }) {
+  return (
+    <div className="artigo-header">
+      <section className="textual">
+        <h1>{titulo}</h1>
+
+        <article className="audio-control">
+        </article>
+
+        <article className="resumo">
+          <h2>Resumo</h2>
+
+          <p>{resumo}</p>
+        </article>
+      </section>
+
+      <section className="visual">
+        <Image src="/placeholder.jpg" width={300} height={240} alt="Placeholder" />
+        <Image src="/placeholder.jpg" width={300} height={240} alt="Placeholder" />
+        <Image src="/placeholder.jpg" width={300} height={480} alt="Placeholder" />
+      </section>
+    </div>
+  );
+}
